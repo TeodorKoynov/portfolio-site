3f7448efd057c878e2e279debf77528b740f5eed
TeodorKoynov <teodor.koynov2005@gmail.com>
2023-06-16 00:45:34 +0300

    hero section and canvas loader

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsC
 export default function Home() {
     return (
         <div className={"relative z-0 bg-primary"}>
-            <div className={"bg-hero-pattern bg-cover bg-no-repeat bg-center"}>
+            <div className={"bg-hero-pattern hero-pattern hero-pattern bg-cover bg-no-repeat bg-center"}>
                 <Navbar/>
                 <Hero/>
             </div>
---
+++ src/components/canvas/CanvasLoader.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import {Html, useProgress} from "@react-three/drei";
+
+export const CanvasLoader = () => {
+
+    const {progress} = useProgress();
+
+    const divStyles = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+    }
+
+    const pStyles = {
+        fontSize: 16,
+        marginTop: 40,
+        fontWeight: 800,
+        color: "#F1F1F1",
+    }
+
+    return (
+        <Html center as="div" style={divStyles}>
+            <span className="canvas-loader"></span>
+            <p style={pStyles}> {progress.toFixed(2)}% </p>
+        </Html>
+    );
+};
